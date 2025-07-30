5cea73a777019d266f9b2f90761c38270a02b18b
Chris <chris@norr.is>
2025-07-30 19:16:03 +0100

    clean up example -- remove duplicate code

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,42 +4,7 @@ use dioxus_icons::icons::heroicons::academic_cap_outline_24;
 use dioxus_icons::icons::materialicons::action::icons::accessibility_24px;
 
 fn main() {
-    dioxus::launch(App_);
-}
-
-#[component]
-fn App_() -> Element {
-    rsx!{
-        div {
-            "Dioxus Icons example"
-            
-            div {
-                style: "margin: 20px 0;",
-                "Heroicons: academic_cap_outline_24"
-                
-                academic_cap_outline_24 {}
-                
-                academic_cap_outline_24 {
-                    width: "50",
-                    height: "50",
-                    fill: "red"
-                }
-            }
-            
-            div {
-                style: "margin: 20px 0;",
-                "Material Icons: accessibility_24px"
-                
-                accessibility_24px {}
-                
-                accessibility_24px {
-                    fill: "none",
-                    stroke_width: "2",
-                    stroke: "orange"
-                }
-            }
-        }
-    }
+    dioxus::launch(App);
 }
 
 #[component]
@@ -67,7 +32,6 @@ fn App() -> Element {
 
                 accessibility_24px {
                     fill: "none",
-
                     stroke_width: "2",
                     stroke: "orange"
                 }
